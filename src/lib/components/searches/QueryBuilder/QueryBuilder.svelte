b4669e3513cec3c687e21164b5d2291d1696291e
yurdm <yd@atrocore.com>
2026-08-17 12:22:50 +0300

    Sidebar styling: thin scrollbar, sticky footer spacing fixes

--- src/lib/components/searches/QueryBuilder/QueryBuilder.svelte
+++ src/lib/components/searches/QueryBuilder/QueryBuilder.svelte
@@ -1449,7 +1449,8 @@
         background-color: var(--sidebar-color);
         margin-top: 10px;
         padding-top: 10px;
-        padding-bottom: 10px;
+        padding-bottom: 20px;
+        margin-bottom: -20px;
         position: sticky;
         bottom: 0;
         left: 0;
